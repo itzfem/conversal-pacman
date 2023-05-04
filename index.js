c53f0dd225f744caf871c0937ce750d824d7271e
femke valckenier <70921541+itzfem@users.noreply.github.com>
2023-05-04 11:00:42 +0200

    update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Boundary {
+    static width = 40;
+    static height = 40;
     constructor({position}) {
         this.position = position;
         this.width = 40;
@@ -17,11 +19,61 @@ class Boundary {
     }
 }
 
-const boundaries = [
-    new Boundary({
-       position: {
-        x: 0,
-        y: 0
-       } 
-    })
+const pacmanImg = new Image()
+pacmanImg.src = "img/conversallogo.png";
+
+class Player {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 10
+    }
+
+    draw() {
+        c.beginPath();
+        c.drawImage(pacmanImg, this.position.x, this.position.y);
+        c.closePath();
+    }
+}
+
+const map = [
+    ['-', '-', '-', '-', '-', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', '-', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', '-', '-', '-', '-', '-'],
 ]
+const boundaries = []
+const player = new Player({
+    position: {
+        x: 40,
+        y: 40
+    },
+    velocity: {
+        x: 0, 
+        y: 0
+    }
+})
+
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: Boundary.width * j,
+                            y: Boundary.height * i
+                        }
+                    })
+                )
+                break;
+        }
+    })
+})
+
+boundaries.forEach(boundary => {
+    boundary.draw();
+})
+
+player.draw();
