a5ea56d8ca6c31b3e9565fa8ef7786cbad6ba8c3
femke valckenier <70921541+itzfem@users.noreply.github.com>
2023-05-09 09:37:51 +0200

    update

--- index.js
+++ index.js
@@ -26,14 +26,23 @@ class Player {
     constructor({position, velocity, image}) {
         this.position = position;
         this.velocity = velocity;
-        this.radius = 10
+        this.radius = 15
         this.image = image;
     }
 
     draw() {
         c.beginPath();
-        c.drawImage(this.image, this.position.x, this.position.y);
+        c.drawImage(this.image, this.position.x - this.radius, this.position.y - this.radius, this.radius * 2, this.radius * 2);
+        //c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        //c.fillStyle = 'yellow';
+        //c.fill();
         c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 }
 
@@ -47,8 +56,8 @@ const map = [
 const boundaries = []
 const player = new Player({
     position: {
-        x: 40,
-        y: 40
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
     },
     velocity: {
         x: 0, 
@@ -56,6 +65,23 @@ const player = new Player({
     },
     image: pacmanImg,
 })
+
+const keys = {
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    }
+}
+
+let lastKey = '';
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
@@ -69,13 +95,73 @@ map.forEach((row, i) => {
                         }
                     })
                 )
-                break;
+            break;
         }
     })
 })
 
-boundaries.forEach(boundary => {
-    boundary.draw();
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    boundaries.forEach((boundary) => {
+        boundary.draw();
+    })
+    player.update();
+    player.velocity.x = 0
+    player.velocity.y = 0
+
+    if(keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
+        player.velocity.y = -5
+    } else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
+        player.velocity.x = -5
+    } else if (keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
+        player.velocity.y = 5
+    } else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
+        player.velocity.x = 5
+    }
+}
+
+pacmanImg.onload = () => {
+    animate();
+}
+
+addEventListener('keydown', function(event) {
+    event.preventDefault();
+    switch(event.key) {
+            case 'ArrowUp':
+                keys.ArrowUp.pressed = true;
+                lastKey = 'ArrowUp';
+                break;
+            case 'ArrowLeft':
+                keys.ArrowLeft.pressed = true;
+                lastKey = 'ArrowLeft';
+                break;
+            case 'ArrowDown':
+                keys.ArrowDown.pressed = true;
+                lastKey = 'ArrowDown';
+                break;
+            case 'ArrowRight':
+                keys.ArrowRight.pressed = true;
+                lastKey = 'ArrowRight';
+                break;
+        }
 })
 
-player.draw();
+addEventListener('keyup', function(event) {
+    event.preventDefault();
+    switch(event.key) {
+            case 'ArrowUp':
+                keys.ArrowUp.pressed = false;
+                break;
+            case 'ArrowLeft':
+                keys.ArrowLeft.pressed = false;
+                break;
+            case 'ArrowDown':
+                keys.ArrowDown.pressed = false;
+                break;
+            case 'ArrowRight':
+                keys.ArrowRight.pressed = false;
+                break;
+        }
+})
+
